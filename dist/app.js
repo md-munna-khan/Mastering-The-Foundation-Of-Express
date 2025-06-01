4c0d9a74eaa15956a172cb8add33f9b1182bf7f7
md.munna <mk8761174@gmail.com>
2025-06-02 00:39:51 +0600

    14-3 What is parsers, request and response object

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express_1.default.json());
+const filePath = path_1.default.join(__dirname, "../db/todo.json");
+app.get("/", (req, res) => {
+    res.send("welcome to todos apps");
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.get("/todos", (req, res) => {
+    const data = fs_1.default.readFileSync(filePath, { encoding: "utf-8" });
+    console.log(data);
+    res.json(data);
 });
+app.post("/todos/create-todo", (req, res) => {
+    const { title, body } = req.body;
+    console.log(title, body);
+    res.send("welcome to todos apps");
+});
+exports.default = app;
+/**
+ * Basic File structure
+ * server - server handling like - starting, closing error handling of server. only related to server
+ * app file - routing handle, middleware, route related error
+ * app folder - app business logic handling like create read update delete, database related works
+ */ 
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,30 @@
 import express, { Application, Request, Response } from "express";
+import fs from "fs";
+import path from "path";
 const app: Application = express();
 
+app.use(express.json());
+
+const filePath = path.join(__dirname, "../db/todo.json");
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+
+  res.send("welcome to todos apps");
 });
+
+app.get("/todos", (req: Request, res: Response) => {
+   const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+   console.log(data)
+  res.json(data)
+});
+
+app.post("/todos/create-todo", (req: Request, res: Response) => {
+const {title,body}= req.body;
+console.log(title,body)
+  res.send("welcome to todos apps");
+});
+
+
+
 
 export default app;
 
